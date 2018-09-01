a27ab722d4060d35467cb24889c3f683bbbe5ffb
MD Naim Uddin <thanvirdx919@gmail.com>
2018-09-01 06:21:32 +0000

    added comment capabilties and some bootstrap styling

---
+++ seeds.js
@@ -0,0 +1,57 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment   = require("./models/comment");
+
+var data = [
+        {
+            name: "Clouds Rest",
+            image: "https://images.unsplash.com/photo-1530375323520-248ebdaa967f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d0629c47a367b54fb1ebdcbeb6ad28f5&auto=format&fit=crop&w=500&q=60",
+            description: "Proin pharetra nisi ac purus condimentum venenatis. Curabitur suscipit varius porttitor. Sed ligula ligula, hendrerit quis turpis vitae, pretium commodo sapien. Sed interdum sagittis mattis. Curabitur a mi tristique, condimentum quam non, ultricies lacus. Donec commodo volutpat blandit. In eget ligula quam. Sed egestas interdum hendrerit."
+        },
+        {
+            name: "Starry Night",
+            image: "https://images.unsplash.com/photo-1530102900583-143a3be5d43f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=be72236f0cb67315dceb05d2dfb94710&auto=format&fit=crop&w=500&q=60",
+            description: "Proin pharetra nisi ac purus condimentum venenatis. Curabitur suscipit varius porttitor. Sed ligula ligula, hendrerit quis turpis vitae, pretium commodo sapien. Sed interdum sagittis mattis. Curabitur a mi tristique, condimentum quam non, ultricies lacus. Donec commodo volutpat blandit. In eget ligula quam. Sed egestas interdum hendrerit."
+        },
+        {
+            name: "Fire Island",
+            image: "https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1a7383ad093ffea99d373681b9974056&auto=format&fit=crop&w=500&q=60",
+            description: "Proin pharetra nisi ac purus condimentum venenatis. Curabitur suscipit varius porttitor. Sed ligula ligula, hendrerit quis turpis vitae, pretium commodo sapien. Sed interdum sagittis mattis. Curabitur a mi tristique, condimentum quam non, ultricies lacus. Donec commodo volutpat blandit. In eget ligula quam. Sed egestas interdum hendrerit."
+        },
+    ]
+
+function seedDB(){
+    //remove  all campgrounds
+    Campground.remove({}, function(err){
+        if(err){
+            console.log(err)
+        } else {
+            console.log("removed campgrounds!!")
+            //add some campgrounds
+                data.forEach(function(seed){
+                    Campground.create(seed, function(err, campground){
+                        if(err){
+                            console.log(err)
+                        } else {
+                            console.log("added a campground")
+                            Comment.create({
+                                text: "This place is great, but i wish there was internet",
+                                author: "md"
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err)
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("created new comment")
+                                }
+                            })
+                        }
+                    })
+                })
+         }
+    })
+    
+}
+
+module.exports = seedDB;
--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 var express         = require("express"),
      app            = express(),
      bodyParser     = require("body-parser"),
-     mongoose       = require("mongoose");
+     mongoose       = require("mongoose"),
+     Campground     = require("./models/campground"),
+     seedDB         = require("./seeds"),
+     Comment        = require("./models/comment")
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+seedDB();
 
 
-// schema setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String
-});
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //     {
 //         name: "Bear Mountain",
-//         image: "https://images.unsplash.com/photo-1519095614420-850b5671ac7f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=63ffaa8c9b9aca319b57204b5d620f56&auto=format&fit=crop&w=500&q=60"
+//         image: "https://images.unsplash.com/photo-1519095614420-850b5671ac7f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=63ffaa8c9b9aca319b57204b5d620f56&auto=format&fit=crop&w=500&q=60",
+//         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution",
 //     }, function(err, campground){
 //         if(err){
 //             console.log(err);
@@ -34,36 +33,86 @@ app.get("/", function(req, res) {
     res.render("landing")
 })
 
-app.get("/campgrounds", function(req, res) {
+app.get("/index", function(req, res) {
     //get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
         if(err) {
             console.log(err);
         } else {
-            res.render("campgrounds", { campgrounds: allCampgrounds });
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
         }
     })
 })
 
-app.post("/campgrounds", function(req, res){
+app.post("/index", function(req, res){
     //get data from form 
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = { name: name, image: image }; 
+    var description = req.body.description;
+    var newCampground = { name: name, image: image, description: description }; 
     
     //create new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreate){
         if (err){
             console.log(err);
         } else {
-            res.redirect("/campgrounds");
+            res.redirect("campgrounds/index");
         }
     })
 })
 
-app.get("/campgrounds/new", function(req, res) {
-    res.render("new")
+//show form to create new campground
+app.get("/index/new", function(req, res) {
+    res.render("campgrounds/new")
+});
+
+//show a single campground info
+app.get("/index/:id", function(req, res) {
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/show", { campground: foundCampground });
+        }
+    })
+    
 })
+
+
+//=======================
+//COMENTS ROUTES
+//=======================
+app.get("/index/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err)
+        } else {
+            res.render("comments/new", { campground: campground });
+        }
+    })
+})
+
+app.post("/index/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err){
+            console.log(err)
+            res.redirect("/index")
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err)
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/index/" + campground._id);
+                }
+            })
+        }
+    })
+})
+
+
 
 //dont mess them for no reason
 app.get("*", function(req, res){
